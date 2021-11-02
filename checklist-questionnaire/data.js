1bd43c229691916dca28a7646e4a3bc7f34d8d0e
ZL <zinovijs.lipinskis@emergn.com>
2021-11-02 19:06:25 +0200

    browse questions

---
+++ checklist-questionnaire/data.js
@@ -0,0 +1,57 @@
+let questionnaire = {
+  questions: [
+    {
+      id:1,
+      topic:'value',
+      title:'The team exists to fulfil a clearly articulated vision or goal',
+      text:'Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      answer:0,
+      comment:''
+    }
+    ,
+    {
+      id:2,
+      topic:'value',
+      title:'Second question',
+      text:'Question text',
+      answer:0,
+      comment:''
+    }
+    ,
+    {
+      id:3,
+      topic:'flow',
+      title:'Third question',
+      text:'Question text',
+      answer:0,
+      comment:''
+    }
+    ,
+    {
+      id:4,
+      topic:'flow',
+      title:'Fourth question',
+      text:'Question text',
+      answer:0,
+      comment:''
+    }
+    ,
+    {
+      id:5,
+      topic:'quality',
+      title:'Fifth question',
+      text:'Question text',
+      answer:0,
+      comment:''
+    }
+    ,
+    {
+      id:6,
+      topic:'quality',
+      title:'Sixth question',
+      text:'Question text',
+      answer:0,
+      comment:''
+    }
+  ]
+};
