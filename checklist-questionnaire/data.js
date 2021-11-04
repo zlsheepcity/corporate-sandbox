1a056f09b2691f8c2973e223bf88d5eb4c061682
ZL <zinovijs.lipinskis@emergn.com>
2021-11-04 11:09:26 +0200

    update 21 questions

--- checklist-questionnaire/data.js
+++ checklist-questionnaire/data.js
@@ -1,50 +1,171 @@
 let questionnaire = {
+  goingFutherStartsFrom: 12,
   questions: [
  {
    "id": 1,
    "topic": "value",
-   "title": "The team exists to fulfil a clearly articulated vision or goal",
-   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+   "title": "A working solution is delivered in 90 days or less after taking a decision to meet a business need (when achieved, go lower – for new work, aim for less than 30 days)\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.1",
    "answer": "",
    "comment": ""
  },
  {
    "id": 2,
    "topic": "value",
-   "title": "Second question",
-   "text": "Question text",
+   "title": "The project/team/group exists to fulfil a clearly articulated vision or goal\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.2",
    "answer": "",
    "comment": ""
  },
  {
    "id": 3,
-   "topic": "flow",
-   "title": "Third question",
-   "text": "Question text",
+   "topic": "value",
+   "title": "All top-level demand increments have an associated business purpose which is testable\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.3",
    "answer": "",
    "comment": ""
  },
  {
    "id": 4,
-   "topic": "flow",
-   "title": "Fourth question",
-   "text": "Question text",
+   "topic": "value",
+   "title": "New demand is either rejected or admitted soon after it arrives (ideally within a week)\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.4",
    "answer": "",
    "comment": ""
  },
  {
    "id": 5,
-   "topic": "quality",
-   "title": "Fifth question",
-   "text": "Question text",
+   "topic": "value",
+   "title": "All demand is owned by an empowered business representative who negotiates its priority\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.5",
    "answer": "",
    "comment": ""
  },
  {
    "id": 6,
+   "topic": "value",
+   "title": "Someone with business knowledge who represents the customer is always available\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.6",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 7,
+   "topic": "value",
+   "title": "Desired outcomes change and are updated during delivery\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.7",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 8,
+   "topic": "flow",
+   "title": "Demand is structured and the flow of related work is visualized, managed and measured end-to-end\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.8",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 9,
+   "topic": "flow",
+   "title": "Work is pulled from one or more dynamically prioritized backlogs only when there is capacity to do so\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.9",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 10,
+   "topic": "flow",
+   "title": "Work always starts within a few days of demand having been pulled from a dynamically prioritized backlog\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.10",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 11,
+   "topic": "flow",
+   "title": "The number of items being worked on is actively managed in at least one location of the flow\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.11",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 12,
+   "topic": "flow",
+   "title": "The level and impact of technical debt is measured and its repayment is prioritized alongside other items of work",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.12",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 13,
+   "topic": "flow",
+   "title": "Work is broken down such that completing the largest items never takes longer that 2 weeks",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.13",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 14,
+   "topic": "flow",
+   "title": " Everyone has the toolset they deem necessary to work effectively",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.14",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 15,
    "topic": "quality",
-   "title": "Sixth question",
-   "text": "Question text",
+   "title": "When a feature is completed, it is demonstrated to the customer for feedback",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.15",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 16,
+   "topic": "quality",
+   "title": "Feedback about whether a feature is releasable is obtained within the delivery period (e.g. a timebox)",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.16",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 17,
+   "topic": "quality",
+   "title": "Developers get near immediate feedback about whether changes they are making work or not",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.17",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 18,
+   "topic": "quality",
+   "title": "A defined set of metrics on code quality in digital solutions is collected, reported on and monitored",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.18",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 19,
+   "topic": "quality",
+   "title": "Everyone is in the habit of using collaborative/creative working sessions and shares learning regularly\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.19",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 20,
+   "topic": "quality",
+   "title": "Time for reflection is regularly scheduled to capture learning and effect improvements in ways of working\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.20",
+   "answer": "",
+   "comment": ""
+ },
+ {
+   "id": 21,
+   "topic": "quality",
+   "title": "Everyone proactively strives to shorten feedback cycles\n",
+   "text": "Some explanation about this practice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.21",
    "answer": "",
    "comment": ""
  }
